557633618a2b4834e67b0be74aac194a9b41d900
kampamocha <diego.campos.sobrino@gmail.com>
2017-07-13 00:28:54 -0500

    Cálculo de estadístico I de Moran para autocorrelación espacial global

--- js/chagas.js
+++ js/chagas.js
@@ -52,53 +52,244 @@ window.onload = function() {
   };
 
 //Calcula esquinas
-  var calcCorners = function(local_key) {
-    // var corners = {};
-    // for(var i = 1, n = data_layers.length; i < n; i++) {
-    //   var source = data_layers[i].getSource();
-    //   var features = source.getFeatures();
-    var center = towns[local_key].center;
-    // }
-    var wgs84Sphere= new ol.Sphere(6378137);
-    var c1 = ol.proj.transform(center, 'EPSG:3857', 'EPSG:4326');
-    //var c2 = ol.proj.transform(old_center, 'EPSG:3857', 'EPSG:4326');
-    //console.log(c1);
-    //console.log(wgs84Sphere.haversineDistance(c1, c2));
-    var distance = towns[local_key].distance;
+  // var calcCorners = function(local_key) {
+  //   // var corners = {};
+  //   // for(var i = 1, n = data_layers.length; i < n; i++) {
+  //   //   var source = data_layers[i].getSource();
+  //   //   var features = source.getFeatures();
+  //   var center = towns[local_key].center;
+  //   // }
+  //   var wgs84Sphere= new ol.Sphere(6378137);
+  //   var c1 = ol.proj.transform(center, 'EPSG:3857', 'EPSG:4326');
+  //   //var c2 = ol.proj.transform(old_center, 'EPSG:3857', 'EPSG:4326');
+  //   //console.log(c1);
+  //   //console.log(wgs84Sphere.haversineDistance(c1, c2));
+  //   var distance = towns[local_key].distance;
 
-    var corners = {
-      up: wgs84Sphere.offset(c1, distance, 0)[1],
-      down: wgs84Sphere.offset(c1, -distance, 0)[1],
-      right: wgs84Sphere.offset(c1, distance, Math.PI / 2)[0],
-      left: wgs84Sphere.offset(c1, -distance, Math.PI / 2)[0]
-    }
+  //   var corners = {
+  //     up: wgs84Sphere.offset(c1, distance, 0)[1],
+  //     down: wgs84Sphere.offset(c1, -distance, 0)[1],
+  //     right: wgs84Sphere.offset(c1, distance, Math.PI / 2)[0],
+  //     left: wgs84Sphere.offset(c1, -distance, Math.PI / 2)[0]
+  //   }
 
-    // var max_lat = wgs84Sphere.offset(c1, 2500, 0);
-    // var min_lat = wgs84Sphere.offset(c1, -2500, 0);
-    // var max_lon = wgs84Sphere.offset(c1, 2500, Math.PI / 2);
-    // var min_lon = wgs84Sphere.offset(c1, -2500, Math.PI / 2);    
+  //   // var max_lat = wgs84Sphere.offset(c1, 2500, 0);
+  //   // var min_lat = wgs84Sphere.offset(c1, -2500, 0);
+  //   // var max_lon = wgs84Sphere.offset(c1, 2500, Math.PI / 2);
+  //   // var min_lon = wgs84Sphere.offset(c1, -2500, Math.PI / 2);    
 
-    return corners;
-  }
+  //   return corners;
+  // };
   
 // Make a square polygon
-  var makeRegion = function(corners) {
-    var ring = [
-      [corners.left, corners.up],
-      [corners.right, corners.up],
-      [corners.right, corners.down],
-      [corners.left, corners.down],
-      [corners.left, corners.up]
-    ];
+  // var makeRegion = function(corners) {
+  //   var ring = [
+  //     [corners.left, corners.up],
+  //     [corners.right, corners.up],
+  //     [corners.right, corners.down],
+  //     [corners.left, corners.down],
+  //     [corners.left, corners.up]
+  //   ];
 
-    var polygon = new ol.geom.Polygon([ring]);
+  //   var polygon = new ol.geom.Polygon([ring]);
 
-    polygon.transform('EPSG:4326', 'EPSG:3857');
+  //   polygon.transform('EPSG:4326', 'EPSG:3857');
 
-    return polygon;
-  }
+  //   return polygon;
+  // };
 
+// Calculate Moran's I global statistic
+  var calcMoranI = function(local_key, distance) {
+    var localidad = towns[local_key].name;
+    var features = getTownFeatures(localidad);
+    var N = features.length;
+    var infected = 0;
+    var non_infected = 0;
+    var empty = 0;
+    var x = [];
 
+    for (var i = 0; i < N; i++) {
+      var feature = features[i];
+      var captura = feature.get('Captura');
+      var cruzi = feature.get('Dx_T_cruzi');
+
+      if (!captura) {
+        empty++;
+        x[i] = 0;
+      } else if (cruzi == 'Positivo') {
+        infected++;
+        x[i] = 1;
+      } else {
+        non_infected++;
+        x[i] = 0;
+      }
+    }
+
+    var m = infected / N;
+    var w = calcWeightMatrix(features, distance);
+    var W = 0;
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+        W = W + w[i][j];
+      }
+    }
+
+    var num = 0;
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+        num += w[i][j] * (x[i] - m) * (x[j] - m);
+      }
+    }    
+
+    var den = 0;
+    for (i = 0; i < N; i++) {
+      den += (x[i] - m) * (x[i] - m);
+
+    }
+
+    // I statistic
+    var stat = N / W * num / den;
+    // Expected value
+    var E = -1 / (N-1);
+    // Variance
+    var S1 = 0;
+    for (var i = 0; i < N; i++) {
+      for (var j = 0; j < N; j++) {
+        S1 += Math.pow(w[i][j] + w[j][i], 2);
+      }
+    }
+    S1 = S1 / 2;
+
+    var S2 = 0
+    for (var i = 0; i < N; i++) {
+      var S2i = 0;
+      for (var j = 0; j < N; j++) {
+        S2i += w[i][j] + w[j][i];
+      }
+      S2 += (S2i * S2i);
+    }
+
+    var S3 = 0;
+    var num = 0;
+    var den = 0;
+    for (var i = 0; i < N; i++) {
+      num += Math.pow(x[i] - m, 4);
+      den += Math.pow(x[i] - m, 2);
+    }
+    num = num / N;
+    den = Math.pow(den / N, 2);
+    S3 = num / den;
+
+    var S4 = (N*N - 3*N + 3)*S1 - N*S2 + 3*W*W;
+
+    var S5 = (N*N - N)*S1 - 2*N*S2 + 6*W*W;
+
+    var Var = (N*S4 - S3*S5) / ((N-1)*(N-2)*(N-3)*W*W) - E*E;
+    // Z score
+    var Z = (stat - E) / Math.sqrt(Var);
+
+    // p value
+    var p = 1 - GetZPercent(Z);
+
+    return { stat: stat, E: E, Var: Var, Z: Z, p: p };
+  };
+
+// Get features from town
+  var getTownFeatures = function(localidad) {
+    var tmpSource = new ol.source.Vector({});
+    var tmpLayer = new ol.layer.Vector({ source: tmpSource });
+
+    for (var i = 0, n = data_layers.length; i < n; i++) {
+      var source = data_layers[i].getSource();
+      var features = source.getFeatures();
+
+      for (var j = 0, featuresNumber = features.length; j < featuresNumber; j++) {
+        var feature = features[j];
+        if (feature.get('Localidad') == localidad) {
+          tmpSource.addFeature(feature);
+        }
+      }
+      
+    }
+
+    return tmpSource.getFeatures();
+  };
+
+// Calculate weight matrix with distance decay function
+  var calcWeightMatrix = function(features, distance) {
+    var wgs84Sphere= new ol.Sphere(6378137);
+
+    var N = features.length;
+
+    var matrix = [];
+    for (var i = 0; i < N; i++) {
+      matrix[i] = [];
+    }
+
+    for (var i = 0; i < N; i++) {
+
+      var i_coord = features[i].getGeometry().getCoordinates();
+      var i_point = ol.proj.transform(i_coord, 'EPSG:3857', 'EPSG:4326');
+
+      matrix[i][i] = 0;
+
+      for (var j = i+1; j < N; j++) {
+
+        var j_coord = features[j].getGeometry().getCoordinates();
+        var j_point = ol.proj.transform(j_coord, 'EPSG:3857', 'EPSG:4326');
+        var d_ij = wgs84Sphere.haversineDistance(i_point, j_point);
+
+        var w_ij = 0;
+        if (d_ij < distance) {
+          //w_ij = (distance - d_ij) / distance;
+          w_ij = 1;
+        }
+
+        matrix[i][j] = w_ij;
+        matrix[j][i] = w_ij;
+      }
+    }
+    return matrix;
+  };
+
+  // Round with decimal places
+  var myRound = function(number, precision) {
+    var factor = Math.pow(10, precision);
+    var tmpNumber = number * factor;
+    var rndTmpNumber = Math.round(tmpNumber);
+    return rndTmpNumber / factor;
+  };
+
+  // Calculate p value from Z score
+  function GetZPercent(z) 
+  {
+    //z == number of standard deviations from the mean
+
+    //if z is greater than 6.5 standard deviations from the mean
+    //the number of significant digits will be outside of a reasonable 
+    //range
+    if ( z < -6.5)
+      return 0.0;
+    if( z > 6.5) 
+      return 1.0;
+
+    var factK = 1;
+    var sum = 0;
+    var term = 1;
+    var k = 0;
+    var loopStop = Math.exp(-23);
+    while(Math.abs(term) > loopStop) 
+    {
+      term = .3989422804 * Math.pow(-1,k) * Math.pow(z,k) / (2 * k + 1) / Math.pow(2,k) * Math.pow(z,k+1) / factK;
+      sum += term;
+      k++;
+      factK *= k;
+
+    }
+    sum += 0.5;
+
+    return sum;
+  }  
 ////////////////////////////////////////////////////////////////
 // EXECUTION
 ////////////////////////////////////////////////////////////////
@@ -312,7 +503,8 @@ window.onload = function() {
   // Select features
   $('#select-localidad').change(function () {
     var local_key = $(this).val();
-    //console.log(local_key);
+    // clean stats
+    $('#moranI').html('');
 
     for(var i = 0, n = data_layers.length; i < n; i++) {
       var source = data_layers[i].getSource();
@@ -331,34 +523,49 @@ window.onload = function() {
 
   // Calculate statistic
   $('#calc-btn').click(function () {
-    var corners = calcCorners($('#select-localidad').val());
-    var polygon = makeRegion(corners);
-    var feature = new ol.Feature(polygon);
-    var vectorSource = new ol.source.Vector();
-    vectorSource.addFeature(feature);
-    var vectorLayer = new ol.layer.Vector({
-      source: vectorSource
-    });
-    map.addLayer(vectorLayer);
+    var local_key = $('#select-localidad').val();
+    var distance = $('#distance-number').val();
+    var I = calcMoranI(local_key, distance);
 
-    console.log(corners);
-    var resolution = $('#select-resolution').val() * 10;
-    var dx = (corners.left - corners.right) / resolution;
-    var dy = (corners.up - corners.down) / resolution;
-
-    x = corners.left;
-    y = corners.up;
-    for(i = 0; i <= resolution; i++) {
-      x = corners.left - i * dx;
-      for(j = 0; j <= resolution; j++) {
-        y = corners.up - j * dy;
-        console.log("**");
-        console.log(x);
-        console.log(y);
-      }
-    }
-
+    var data = '<strong>I: </strong>' + myRound(I.stat, 4) + '<br>';
+    data += '<strong>E(I): </strong>' + myRound(I.E, 4) + '<br>';
+    data += '<strong>Var(I): </strong>' + myRound(I.Var, 4) + '<br>';
+    data += '<strong>Z(I): </strong>' + myRound(I.Z, 4) + '<br>';
+    data += '<strong>p: </strong>' + myRound(I.p, 4) + '<br>'; 
+    $('#moranI').html(data);
   });
+  
+  //// Calculate with regions
+  // $('#calc-btn').click(function () {
+  //   var corners = calcCorners($('#select-localidad').val());
+  //   var polygon = makeRegion(corners);
+  //   var feature = new ol.Feature(polygon);
+  //   var vectorSource = new ol.source.Vector();
+  //   vectorSource.addFeature(feature);
+  //   var vectorLayer = new ol.layer.Vector({
+  //     source: vectorSource
+  //   });
+  //   map.addLayer(vectorLayer);
+
+  //   console.log(corners);
+  //   var resolution = $('#select-resolution').val() * 10;
+  //   var dx = (corners.left - corners.right) / resolution;
+  //   var dy = (corners.up - corners.down) / resolution;
+
+  //   x = corners.left;
+  //   y = corners.up;
+  //   for(i = 0; i <= resolution; i++) {
+  //     x = corners.left - i * dx;
+  //     for(j = 0; j <= resolution; j++) {
+  //       y = corners.up - j * dy;
+  //       console.log("**");
+  //       console.log(x);
+  //       console.log(y);
+  //     }
+  //   }
+  //  });
+
+
   //update styles
   // var tmp_source = data_layers[0].getSource();
   // var tmp_features = tmp_source.getFeatures();
